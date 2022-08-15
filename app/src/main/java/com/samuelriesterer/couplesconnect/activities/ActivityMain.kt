d7940945e894260328a04ff7e275a3579ddb02dd
Samuel Riesterer <samuel.riesterer@wsu.edu>
2022-08-15 11:01:53 -0700

    Update

--- app/src/main/java/com/samuelriesterer/couplesconnect/activities/ActivityMain.kt
+++ app/src/main/java/com/samuelriesterer/couplesconnect/activities/ActivityMain.kt
@@ -24,6 +24,7 @@ import com.samuelriesterer.couplesconnect.general.Settings
 import com.samuelriesterer.couplesconnect.interfaces.InterfaceMain
 import java.util.*
 
+
 class ActivityMain : AppCompatActivity(), InterfaceMain, NavigationView.OnNavigationItemSelectedListener {
 	private lateinit var appBarConfiguration: AppBarConfiguration
 	private lateinit var binding: ActivityMainBinding
